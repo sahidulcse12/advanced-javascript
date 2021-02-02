8a00b03f1f1d6379f8a76a46e465d6056af4d0ed
sahidulcse12 <m.sahidulislam.14@gmail.com>
2021-02-03 00:26:32 +0600

    midnight

---
+++ array-slice.js
@@ -0,0 +1,12 @@
+
+const nums = [1,2,3,4,5];
+
+// const result = nums.slice(2,5); // 2 no. index theke 5 no. index porjonto dekhabe
+// console.log(result);
+
+// const result = nums.splice(2,5); // 2 no. index theke 5 no. index porjonto dekhabe
+// console.log(result);
+// console.log(nums); // main array change kore....slice korena
+
+const elements = ['Fire', 'Air', 'Water'];
+console.log(elements.join(',')); // comma diye join korbe
